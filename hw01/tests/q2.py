b120a6f21c733132f7db8a513c137b208be166b6
Kinto <kinto@169-231-129-194.wireless.ucsb.edu>
2018-10-10 13:59:54 -0700

    Added HW 1

---
+++ hw01/tests/q2.py
@@ -0,0 +1,26 @@
+test = {
+  'name': 'Question 2_',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> 1 <= admissions_q1 <= 5
+          True
+          >>> 1 <= admissions_q2 <= 3
+          True
+          >>> 1 <= admissions_q3 <= 3
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
