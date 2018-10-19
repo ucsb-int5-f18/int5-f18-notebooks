d53e2cf7c9566eb407d859133ea0c3e65eece56d
nikhilmaram <nike.maram@gmail.com>
2018-10-19 11:16:31 -0700

    Changed output of 6.7

--- lab03/tests/q6_7.py
+++ lab03/tests/q6_7.py
@@ -6,11 +6,13 @@ test = {
       'cases': [
         {
           'code': r"""
-          >>> berkeley_markets.sort(0)
-          MarketName                        | city     | State      | y       | x
-          Downtown Berkeley Farmers' Market | Berkeley | California | 37.8697 | -122.273
-          North Berkeley Farmers' Market    | Berkeley | California | 37.8802 | -122.269
-          South Berkeley Farmers' Market    | Berkeley | California | 37.8478 | -122.272
+          >>> sb_markets.sort(0)
+          MarketName                             | city          | State      | y       | x
+          Harding Elementary School CFM          | Santa Barbara | California | 34.4166 | -119.722
+          Montrose Harvest Market Farmers Market | Santa Barbara | California | 34.4222 | -119.713
+          Santa Barbara Farmers Market           | Santa Barbara | California | 34.4199 | -119.695
+          Santa Barbara Farmers Market           | Santa Barbara | California | 34.4177 | -119.697
+          Santa Barbara La Cumbre Farmers Market | Santa Barbara | California | 34.4378 | -119.747
           """,
           'hidden': False,
           'locked': False
@@ -23,3 +25,4 @@ test = {
     }
   ]
 }
+
