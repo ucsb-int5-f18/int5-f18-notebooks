d03bcc08ed0d99c44143a7f9dcd0f9ee134481d2
Kinto <kinto@192.168.1.13>
2018-10-19 00:49:41 -0700

    Added HW2 and Lab3

---
+++ hw02/tests/q4.py
@@ -0,0 +1,30 @@
+test = {
+  'name': 'Question 4',
+  'points': 5,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> 1 <= soccer_q1 <= 3
+          True
+          >>> 1 <= soccer_q2 <= 3
+          True
+          >>> 1 <= soccer_q3 <= 3
+          True
+          >>> 1 <= soccer_q4 <= 3
+          True
+          >>> 1 <= soccer_q5 <= 3
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
