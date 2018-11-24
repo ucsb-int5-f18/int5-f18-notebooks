f8bf68f66606c96e012edfe8e2e8f80de1db06dd
Kinto <kinto@Kintos-MacBook-Air.local>
2018-11-24 14:15:24 -0800

    Add final project (global population)

---
+++ final-project/global_pop/tests/q14.py
@@ -0,0 +1,31 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # Please use a list of integers from 1 to 5
+          >>> all(x in range(1, 6) for x in set(scatter_statements))
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> set(scatter_statements)
+          {1, 3, 4}
+          """,
+          'hidden': True,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ final-project/global_pop/tests/q9.py
@@ -0,0 +1,31 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # Please use a list of integers from 1 to 6
+          >>> all(x in range(1, 7) for x in set(fertility_statements))
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> set(fertility_statements)
+          {1, 3, 4, 5, 6}
+          """,
+          'hidden': True,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
