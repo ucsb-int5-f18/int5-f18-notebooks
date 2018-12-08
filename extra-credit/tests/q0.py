c3d011b9cc657ad3ab648e93ca4e6af4c05c37b0
Kinto <kinto@Kintos-MacBook-Air.local>
2018-12-07 20:49:47 -0800

    Add tests

---
+++ extra-credit/tests/q0.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 1_1',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # Looks like you haven't started yet
+          False
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
